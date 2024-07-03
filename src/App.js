e57351592164ea261ee1cb6b913e909e0df760aa
Ivan Pavic <ivan96jp@gmail.com>
2024-07-03 10:47:54 +0200

    Added meta tags

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <div className="main-container">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav class="navigation">
+      <img src={"logo.svg"} alt="logo" />
+      <ul className="navigation__links">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Menu</a>
+        </li>
+        <li>
+          <a href="#">Reservations</a>
+        </li>
+        <li>
+          <a href="#">Order Online</a>
+        </li>
+        <li>
+          <a href="#">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
