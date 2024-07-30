65642dad6b868eed2675c61430776a435447b39b
Ivan Pavic <ivan96jp@gmail.com>
2024-07-30 22:04:48 +0200

    Refactored Highlights component

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -8,30 +8,41 @@ import GreekSalad from "../assets/img/greek-salad.jpg";
 import Bruschetta from "../assets/img/bruschetta.jpg";
 import LemonDessert from "../assets/img/lemon-dessert.jpg";
 
+const data = [
+  {
+    imageSrc: GreekSalad,
+    title: "Greek Salad",
+    body: "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheeese, garnished with crunchy garlic and rosemary croutons.",
+    price: "$12.99",
+  },
+  {
+    imageSrc: Bruschetta,
+    title: "Bruschetta",
+    body: "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.",
+    price: "$7.99",
+  },
+  {
+    imageSrc: LemonDessert,
+    title: "Lemon Dessert",
+    body: "This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined.",
+    price: "$5.00",
+  },
+];
+
 const Highlights = () => {
   return (
     <div className="highlights">
       <div className="highlights__container section">
         <ActionHeading title="This Weeks specials!" actionLabel="ONLINE MENU" />
         <div className="highlights__specials">
-          <Card
-            imageSrc={GreekSalad}
-            title="Greek Salad"
-            body="The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheeese, garnished with crunchy garlic and rosemary croutons."
-            price="$12.99"
-          />
-          <Card
-            imageSrc={Bruschetta}
-            title="Bruschetta"
-            body="Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil."
-            price="$7.99"
-          />
-          <Card
-            imageSrc={LemonDessert}
-            title="Lemon Dessert"
-            body="This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined."
-            price="$5.00"
-          />
+          {data.map((item) => (
+            <Card
+              imageSrc={item.imageSrc}
+              title={item.title}
+              body={item.body}
+              price={item.price}
+            />
+          ))}
         </div>
       </div>
     </div>
